d91180f6cd0d9d966b3be7e5775a99b2aa64a23f
Guilherme Polim <88114079+gpolim@users.noreply.github.com>
2023-02-24 17:51:25 -0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -6,10 +6,10 @@ import requests
 #print(df)
 
 # Faz uma solicitação GET para a API
-response = requests.get("https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial=%2701-01-2019%27&@dataFinalCotacao=%2712-31-2025%27&$top=9000&$format=text/csv&$select=cotacaoCompra,cotacaoVenda,dataHoraCotacao")
+api = "https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial=%2701-01-2019%27&@dataFinalCotacao=%2712-31-2025%27&$top=9000&$format=text/csv&$select=cotacaoCompra,cotacaoVenda,dataHoraCotacao"
 
 # Extrai os dados da resposta como uma lista de dicionários
-data = response.json()
+data = pl.read_csv(api)
 
 # Cria um DataFrame Polars a partir dos dados extraídos
 df = pl.DataFrame(data)
