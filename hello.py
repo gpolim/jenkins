92ef5da64785d69ee4368b90a115ec4c81542a18
Guilherme Polim <88114079+gpolim@users.noreply.github.com>
2023-02-24 17:47:41 -0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,5 +1,18 @@
 import polars as pl
+import requests
 
-data = {"a": [1, 2], "b": [3, 4]}
+#data = {"a": [1, 2], "b": [3, 4]}
+#df = pl.DataFrame(data)
+#print(df)
+
+# Faz uma solicitação GET para a API
+response = requests.get('https://jsonplaceholder.typicode.com/todos')
+
+# Extrai os dados da resposta como uma lista de dicionários
+data = response.json()
+
+# Cria um DataFrame Polars a partir dos dados extraídos
 df = pl.DataFrame(data)
+
+# Imprime o DataFrame
 print(df)
