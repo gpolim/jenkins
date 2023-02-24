5e8881ccad6b4f5bde4dc909b06b03236a19aa24
Guilherme Polim <88114079+gpolim@users.noreply.github.com>
2023-02-24 17:53:04 -0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,15 +1,11 @@
-import polars as pl
 import requests
-
-#data = {"a": [1, 2], "b": [3, 4]}
-#df = pl.DataFrame(data)
-#print(df)
+import polars as pl
 
 # Faz uma solicitação GET para a API
-api = "https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo"
+response = requests.get('https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo')
 
 # Extrai os dados da resposta como uma lista de dicionários
-data = pl.read_csv(api)
+data = response.json()
 
 # Cria um DataFrame Polars a partir dos dados extraídos
 df = pl.DataFrame(data)
