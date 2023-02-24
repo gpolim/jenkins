c45c12a4ce0573350b9c477155aaeb8eaf39888e
Guilherme Polim <88114079+gpolim@users.noreply.github.com>
2023-02-24 18:21:08 -0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -14,11 +14,17 @@ print(df)
 
 df2 = df.drop("userId")
 
+# Converta a coluna 'title' em um tipo de dados Utf8
 df2 = df2.with_column(pl.col('title').cast(pl.Utf8))
 
+# Defina uma função personalizada para converter a string em maiúsculas
 def to_uppercase(s: str) -> str:
     return s.upper()
 
-df2['title'] = df2['title'].apply(to_uppercase)
+# Aplique a função personalizada à coluna 'title' usando o método 'apply()'
+series = df2['title'].apply(to_uppercase)
+
+# Crie uma nova coluna com os valores retornados pelo método 'apply()'
+df2 = df2.with_column('title_upper', series)
 
 print(df2)
