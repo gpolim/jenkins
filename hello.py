d9c8074d7de775dbbac8cf10e3604e406fc7cec5
Guilherme Polim <88114079+gpolim@users.noreply.github.com>
2023-02-24 18:00:00 -0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,14 +1,15 @@
 import requests
 import polars as pl
 
-# Faz uma solicitação GET para a API
 response = requests.get('https://jsonplaceholder.typicode.com/todos')
 
-# Extrai os dados da resposta como uma lista de dicionários
 data = response.json()
 
-# Cria um DataFrame Polars a partir dos dados extraídos
+
 df = pl.DataFrame(data)
 
-# Imprime o DataFrame
 print(df)
+
+df2 = df.drop("userID")
+
+print(df2)
