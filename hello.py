2398842c87c9562b0fdf69674cbe20e04cc17bca
Guilherme Polim <88114079+gpolim@users.noreply.github.com>
2023-02-24 18:17:21 -0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -14,6 +14,9 @@ print(df)
 
 df2 = df.drop("userId")
 
+df2 = df2.with_column(pl.col('title').cast(pl.Utf8))
+
+# Converta o conteúdo da coluna 'title' em maiúsculas
 df2['title'] = df2['title'].str_to_upper()
 
 print(df2)
