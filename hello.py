15bfbce698b2b600ba3fa9e549d3d8730f13602c
Guilherme Polim <88114079+gpolim@users.noreply.github.com>
2023-04-28 13:29:19 -0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -12,13 +12,13 @@ df = pl.DataFrame(data)
 
 print(df)
 
-df2 = df.drop("userId")
+#df2 = df.drop("userId")
 
-df2 = df2.with_column(pl.col('title').cast(pl.Utf8))
+#df2 = df2.with_column(pl.col('title').cast(pl.Utf8))
 
-def to_uppercase(s: str) -> str:
-    return s.upper()
+#def to_uppercase(s: str) -> str:
+ #   return s.upper()
 
-series = df2['title'].apply(to_uppercase)
+#series = df2['title'].apply(to_uppercase)
 
-print(series)
+#print(series)
